710b76079a8d37b6d88bd61779345a464f2cb41e
Branko <branko.popovich@live.com>
2014-04-23 08:57:23 +0200

    first test commit

---
+++ BDD/BDD/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BDD
+//
+//  Created by Branko Popovic on 4/23/14.
+//  Copyright (c) 2014 matf. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace::std;
+
+int main(int argc, const char * argv[])
+{
+    cout << "Simple test for git commit and push\n";
+    return 0;
+}
+
